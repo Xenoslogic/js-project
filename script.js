75e03f799bea436835c2644c64bc00e93e64f139
Xenoslogic <sdv666999@gmail.com>
2026-04-21 08:34:04 +0300

    fix-01

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ let title = prompt('Как называется ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
 let screenPrice = +prompt('Сколько будет стоить данная работа?');
 let rollback = 30;
-
-let adaptive = prompt('Нужен ли адаптив на сайте?');
+let adaptive = confirm('Нужен ли адаптив на сайте?');
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
